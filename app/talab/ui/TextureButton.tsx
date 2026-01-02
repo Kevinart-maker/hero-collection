da1b177a124307b263d8d7f261912ff92dded602
Kevin Odeyemi <kevinodeyemi76@gmail.com>
2026-01-02 17:12:43 +0000

    fixed texture-btn

--- app/talab/ui/TextureButton.tsx
+++ app/talab/ui/TextureButton.tsx
@@ -9,7 +9,7 @@ export default function TextureButton() {
     whileHover={{ scale: 1.05 }}
     whileTap={{ scale: 0.95 }}
     transition={{ type: 'spring', stiffness: 400, damping: 30 }}
-    className='bg-[url("/bg.png")] bg-cover bg-center bg-no-repeat w-fit rounded-2xl p-1 ring-1 ring-white shadow-xl shadow-black/20 cursor-pointer group scale-75 md:scale-0'>
+    className='bg-[url("/bg.png")] bg-cover bg-center bg-no-repeat w-fit rounded-2xl p-1 ring-1 ring-white shadow-xl shadow-black/20 cursor-pointer group scale-75 md:scale-100'>
         <div className='rounded-xl bg-white/95 p-1 pl-4 flex items-center gap-5'>
             <span className='font-medium text-[#666]'>Get Started</span>
 
